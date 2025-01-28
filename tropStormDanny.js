2c6be3e813b35a1c3a8657fb7eae9531450918f0
duncanrodriguez-original <72842975+duncanrodriguez-original@users.noreply.github.com>
2025-01-28 14:45:36 -0500

    Add files via upload

---
+++ tropStormDanny.js
@@ -0,0 +1,14 @@
+var danny = 
+{
+"type": "FeatureCollection",
+"name": "tropStormDanny",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "STORMNAME": "INVEST", "DTG": 2021062718.0, "YEAR": 2021.0, "MONTH": "06", "DAY": 27.0, "HHMM": "1800", "MSLP": 1013.0, "BASIN": "al", "STORMNUM": 4.0, "STORMTYPE": "TD", "INTENSITY": 30.0, "SS": 0.0, "LAT": 29.8, "LON": -72.8 }, "geometry": { "type": "Point", "coordinates": [ -72.8, 29.8 ] } },
+{ "type": "Feature", "properties": { "STORMNAME": "INVEST", "DTG": 2021062800.0, "YEAR": 2021.0, "MONTH": "06", "DAY": 28.0, "HHMM": "0000", "MSLP": 1013.0, "BASIN": "al", "STORMNUM": 4.0, "STORMTYPE": "TD", "INTENSITY": 30.0, "SS": 0.0, "LAT": 30.1, "LON": -74.5 }, "geometry": { "type": "Point", "coordinates": [ -74.5, 30.1 ] } },
+{ "type": "Feature", "properties": { "STORMNAME": "INVEST", "DTG": 2021062806.0, "YEAR": 2021.0, "MONTH": "06", "DAY": 28.0, "HHMM": "0600", "MSLP": 1011.0, "BASIN": "al", "STORMNUM": 4.0, "STORMTYPE": "TS", "INTENSITY": 35.0, "SS": 0.0, "LAT": 30.7, "LON": -76.1 }, "geometry": { "type": "Point", "coordinates": [ -76.1, 30.7 ] } },
+{ "type": "Feature", "properties": { "STORMNAME": "FOUR", "DTG": 2021062812.0, "YEAR": 2021.0, "MONTH": "06", "DAY": 28.0, "HHMM": "1200", "MSLP": 1011.0, "BASIN": "al", "STORMNUM": 4.0, "STORMTYPE": "TS", "INTENSITY": 35.0, "SS": 0.0, "LAT": 31.5, "LON": -77.6 }, "geometry": { "type": "Point", "coordinates": [ -77.6, 31.5 ] } },
+{ "type": "Feature", "properties": { "STORMNAME": "DANNY", "DTG": 2021062818.0, "YEAR": 2021.0, "MONTH": "06", "DAY": 28.0, "HHMM": "1800", "MSLP": 1009.0, "BASIN": "al", "STORMNUM": 4.0, "STORMTYPE": "TS", "INTENSITY": 40.0, "SS": 0.0, "LAT": 32.0, "LON": -79.5 }, "geometry": { "type": "Point", "coordinates": [ -79.5, 32.0 ] } },
+{ "type": "Feature", "properties": { "STORMNAME": "DANNY", "DTG": 2021062900.0, "YEAR": 2021.0, "MONTH": "06", "DAY": 29.0, "HHMM": "0000", "MSLP": 1011.0, "BASIN": "al", "STORMNUM": 4.0, "STORMTYPE": "TD", "INTENSITY": 30.0, "SS": 0.0, "LAT": 32.4, "LON": -80.7 }, "geometry": { "type": "Point", "coordinates": [ -80.7, 32.4 ] } }
+]
+}
